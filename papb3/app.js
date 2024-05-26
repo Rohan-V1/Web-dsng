133faed00e86671f8229645d23a682c8fde3ce22
Rohan-V1 <141546560+Rohan-V1@users.noreply.github.com>
2024-05-26 19:01:25 +0530

    .

---
+++ papb3/app.js
@@ -0,0 +1,100 @@
+const quizData = [
+    {
+        question: "Which language runs in a web browser?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "JavaScript",
+        correct: "d",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Central Style Sheets",
+        b: "Cascading Style Sheets",
+        c: "Cascading Simple Sheets",
+        d: "Cars SUVs Sailboats",
+        correct: "b",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hyper Text Markup Language",
+        b: "Hyper Markdown Language",
+        c: "Hyper Machine Language",
+        d: "Helicopter Terminals Motorboats Lamborghinis",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "None of the above",
+        correct: "b",
+    },
+    {
+        question: "What does SVG stand for?",
+        a: "Scalar Vector Graphics",
+        b: "Scale Vector Graphics",
+        c: "Solar Vector Graphics",
+        d: "None of the above",
+        correct: "a",
+    }
+];
+
+const quiz = document.getElementById("quiz");
+const answerElements = document.querySelectorAll(".answer");
+const questionElement = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitButton = document.getElementById("submit");
+const scoreButton = document.getElementById("score");
+let currentQuiz = 0;
+let score = 0;
+
+const deselectAnswers = () => {
+    answerElements.forEach((answer) => (answer.checked = false));
+};
+
+const getSelected = () => {
+    let answer;
+    answerElements.forEach((answerElement) => {
+        if (answerElement.checked) answer = answerElement.id;
+    });
+    return answer;
+};
+
+const loadQuiz = () => {
+    deselectAnswers();
+    const currentQuizData = quizData[currentQuiz];
+    questionElement.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+};
+
+loadQuiz();
+
+submitButton.addEventListener("click", () => {
+    const answer = getSelected();
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) score++;
+        currentQuiz++;
+        if (currentQuiz < quizData.length) loadQuiz();
+        else {
+            quiz.innerHTML = `<h2>You answered ${score}/${quizData.length} questions correctly</h2>
+            <button onclick="disScore();">Score</button><br>
+            <button onclick="history.go(0)">Play Again</button>`;
+        }
+    }
+});
+
+scoreButton.addEventListener("click", () => {
+    window.alert("Your score till now is:" + score * 10);
+});
+
+function disScore() {
+    window.alert("Your final score is:" + score * 10);
+}
